d8065d7b1cec46873efe6857f2b5469bb4c8bfb3
shkityrk <shkityr.k@yandex.ru>
2025-02-05 18:24:09 +0300

    del: cors middleware origins host not need no more

--- auth/main.py
+++ auth/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine
 from app.models import Base
-from app.EnvArrayProcessor import EnvArrayProcessor
 from app.routes import auth_router
 
 # Создаем таблицы
@@ -11,15 +10,10 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Инициализируем список разрешенных источников
-processor = EnvArrayProcessor("../.hosts")
-origins = processor.get_unique_urls()
-print(origins)
-
 # Добавляем CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
